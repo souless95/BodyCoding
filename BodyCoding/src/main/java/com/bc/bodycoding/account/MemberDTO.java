23678b141e00f540ea1d422f6350dc524384cf7d
sy <han38174481@gmail.com>
2023-02-20 18:59:14 +0900

    2/20 세영- 회원가입폼, 멤버insert완료

---
+++ BodyCoding/src/main/java/com/bc/bodycoding/account/MemberDTO.java
@@ -0,0 +1,33 @@
+package com.bc.bodycoding.account;
+
+import java.sql.Date;
+
+import org.apache.ibatis.jdbc.SQL;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+	
+	private String mem_id;
+	private String mem_pass;
+	private String mem_name;
+	private String mem_gender;
+	private String mem_birth;
+	private String mem_phone;
+	private String mem_address;
+	private String mem_type;
+	private String mem_status;
+	private String gym_code;
+	private Float  mem_height;
+	private Float  mem_weight;
+	private String mem_disease;
+	private String mem_purpose;
+	private String mem_interest;
+	private String mem_inflow;
+	private int mem_point;
+	private String mem_career;
+	private String mem_comment;
+	private String mem_img;
+	private Date mem_regidate;
+}

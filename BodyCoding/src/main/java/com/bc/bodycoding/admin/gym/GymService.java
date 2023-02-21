2cd38bbdf01db65503e3c6f94de78d57615aeb3f
seyoung22222 <sae0young@naver.com>
2023-02-21 16:55:08 +0900

    경로변경

--- BodyCoding/src/main/java/com/bc/bodycoding/admin/gym/GymService.java
+++ BodyCoding/src/main/java/com/bc/bodycoding/admin/gym/GymService.java
@@ -10,10 +10,9 @@ import com.bc.bodycoding.MemberDTO;
 @Mapper
 public interface GymService {
 
-	public List<GymDTO> select();
+	public List<MemberDTO> select();
 	public int insert(GymDTO gymDTO);
 	public GymDTO selectOne(GymDTO gymDTO);
 	public int update(GymDTO gymDTO);
 	public int delete(GymDTO gymDTO);
-	public int insertMemberASUB(MemberDTO memberDTO);
 }

9254d5cec33321f56b58f00abf4cb2fe0ac89f38
seyoung22222 <sae0young@naver.com>
2023-02-23 11:56:40 +0900

    2/23 세영 - 카카오 로그인 연결 완료

--- BodyCoding/src/main/java/com/bc/bodycoding/account/AccountService.java
+++ BodyCoding/src/main/java/com/bc/bodycoding/account/AccountService.java
@@ -1,5 +1,7 @@
 package com.bc.bodycoding.account;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import global.dto.MemberDTO;
@@ -9,5 +11,8 @@ public interface AccountService{
 
 	public int insertMember(MemberDTO memberDTO);
 	public String login(MemberDTO memberDTO);
+	public int kakaoinsert(Map<String, String> map);
+	public String kakaoselect(String mem_id);
+	public int kakaoupdate(MemberDTO memberDTO);
 	
 }

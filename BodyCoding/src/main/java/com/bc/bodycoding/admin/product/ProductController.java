6826429d4d3d9017b55a1d39a440d28c85f60dc5
souless95 <yj6807@gmail.com>
2023-02-23 20:57:51 +0900

    Merge branch 'main' of https://github.com/souless95/BodyCoding.git into main

--- BodyCoding/src/main/java/com/bc/bodycoding/admin/product/ProductController.java
+++ BodyCoding/src/main/java/com/bc/bodycoding/admin/product/ProductController.java
@@ -1,10 +1,15 @@
 package com.bc.bodycoding.admin.product;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import global.dto.ProductDTO;
 
@@ -49,7 +54,16 @@ public class ProductController {
 	@RequestMapping(value="/stockList.do")
 	public String slist(Model model) {
 		model.addAttribute("sList", productdao.stockSelect());
-		return "/admin/product/pList";
+		return "/admin/product/stockList";
+	}
+	
+
+	
+	@ResponseBody
+	@RequestMapping(value="/stockUpdate.do", method = RequestMethod.POST)
+	public String sUpdate(Model model, HttpServletRequest req) {
+		System.out.println(req.getParameter("product_stock"));
+		return "/admin/product/stockList";
 	}
 
 }

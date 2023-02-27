b0d06b519de9ec241cc1607f198fb01a1b0b083a
seyoung22222 <sae0young@naver.com>
2023-02-27 17:17:41 +0900

    2/27 세영 - 트레이너 평점, 후기 작성/ 로그인후 가능할수있게 자스 추가

--- BodyCoding/src/main/java/com/bc/bodycoding/main/MemberMainService.java
+++ BodyCoding/src/main/java/com/bc/bodycoding/main/MemberMainService.java
@@ -12,10 +12,11 @@ public interface MemberMainService {
 	public List<MemberDTO> gymlistSelect();
 	public List<MemberDTO> trainerALLlistSelect();
 	public List<MemberDTO> trainerlistSelect(String gym_code);
-	public MemberDTO trainerInfoSelect(MemberDTO memberDTO);
-	public int gradeSelete(String review_subject);
+	public MemberDTO trainerInfoSelect(String mem_id);
+	public Integer gradeSelete(String review_subject);
 	public List<ReviewDTO> reviewSelect(String review_subject);
-	public String gymInfoSelect(MemberDTO memberDTO);
+	public int reviewInsert(ReviewDTO reviewDTO);
+	public String gymInfoSelect(String gym_code);
 	public List<ProductDTO> product_categoryALLSelect();
 	public List<ProductDTO> product_categorySelect(String product_type);
 	public ProductDTO productInfoSelect(int product_idx);

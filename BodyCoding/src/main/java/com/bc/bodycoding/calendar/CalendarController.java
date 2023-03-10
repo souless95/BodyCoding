c95329f7f6ae6d92ab532a0110d9d6625373188c
smreodlsrks <smreodlsrksz@naver.com>
2023-03-10 10:01:09 +0900

    0310 오전커밋 천희수

--- BodyCoding/src/main/java/com/bc/bodycoding/calendar/CalendarController.java
+++ BodyCoding/src/main/java/com/bc/bodycoding/calendar/CalendarController.java
@@ -1,6 +1,13 @@
 package com.bc.bodycoding.calendar;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import global.dto.MemberDTO;
+
+//calendarDTO = lesson table
 @Controller
 public class CalendarController {
 	
@@ -18,49 +28,74 @@ public class CalendarController {
 	
 	@GetMapping("calendar.do")
 	public String fullCalendar(Model model) {
-		model.addAttribute("cList", calendarDAO.loadCList());
+		
+		List<CalendarDTO> cList = calendarDAO.loadCList();
+		List<Map<String, Object>> eventList = new ArrayList<Map<String, Object>>();
+		for (CalendarDTO fullList : cList) {
+		    Map<String, Object> event = new HashMap<String, Object>();
+		    event.put("id", fullList.getLesson_idx());
+		    event.put("title", fullList.getLesson_name());
+		    event.put("start", fullList.getLesson_day().substring(0,10) + "T" + fullList.getLesson_start_time() + ":00+09:00");
+		    event.put("end", fullList.getLesson_day().substring(0,10) + "T" + fullList.getLesson_end_time() + ":00+09:00");
+		    eventList.add(event);
+		}
+		model.addAttribute("cList", eventList);
 		
 		return "fullCalendar";
 	}
 	
+	@GetMapping("/addCalendar1.do")
+	public String addCalendar1(Model model) {
+		return "addCalendar";
+	}
+	
 	@ResponseBody
-	@RequestMapping("/AddCalendar")
-	public String addCalendar(@Param("id") String id, HttpServletRequest req, CalendarDTO calendarDTO){
+	@RequestMapping("/addCalendar2.do")
+	public String addCalendar(@Param("lesson_idx") String lesson_idx, HttpServletRequest req,
+			CalendarDTO calendarDTO, HttpSession session){
 		
-		calendarDTO.setTitle(req.getParameter("title"));
-		calendarDTO.setStart1(req.getParameter("start"));
-		calendarDTO.setEnd(req.getParameter("end"));
-		calendarDTO.setId(id);
+		String time = req.getParameter("start");
+		String[] timeDivided = time.split("T");
+		String time2 = req.getParameter("end");
+		String[] timeDivided2 = time2.split("T");
 		
-		int result = calendarDAO.insertSchedule(calendarDTO);
+		calendarDTO.setLesson_idx(lesson_idx);
+		calendarDTO.setTrainer_id((String)session.getAttribute("UserEmail"));
+		calendarDTO.setLesson_category(req.getParameter("lesson_category"));
+		calendarDTO.setLesson_name(req.getParameter("title"));
+		calendarDTO.setLesson_day(timeDivided[0]);
+		calendarDTO.setLesson_start_time(timeDivided[1].substring(0, 5));
+		calendarDTO.setLesson_end_time(timeDivided2[1].substring(0, 5));
+		calendarDTO.setLesson_capacity(req.getParameter("lesson_capacity"));
+		System.out.println(calendarDTO);
 		
-		if(result==1) {
-	         System.out.println("업데이트 성공!");
-		}
-		System.out.println(calendarDTO.getId());
-		return calendarDTO.getId();
+		calendarDAO.insertSchedule(calendarDTO);
+		
+		return calendarDTO.getLesson_idx();
 	}
 	
 	@ResponseBody
 	@RequestMapping("/deleteCalendar" )
 	public String deleteCalendar(HttpServletRequest req, CalendarDTO calendarDTO){
 		
-		calendarDTO.setId(req.getParameter("lastClickedEventId"));
+		calendarDTO.setLesson_idx(req.getParameter("lastClickedEventId"));
+		calendarDAO.deleteSchedule(calendarDTO);
 		
-		int result = calendarDAO.deleteSchedule(calendarDTO);
-		if(result==1) {
-	         System.out.println("삭제 성공!");
-		}
 		return "calendar.do";
 	}
-	
+		
 	@ResponseBody
 	@RequestMapping("/updateCalendar" )
 	public String updateCalendar(HttpServletRequest req, CalendarDTO calendarDTO){
 		
-		calendarDTO.setId(req.getParameter("id"));
-		calendarDTO.setStart1(req.getParameter("start"));
-		calendarDTO.setEnd(req.getParameter("end"));
+		String time = req.getParameter("start");
+		String[] timeDivided = time.split("T");
+		String time2 = req.getParameter("end");
+		String[] timeDivided2 = time2.split("T");
+		
+		calendarDTO.setLesson_idx(req.getParameter("id"));
+		calendarDTO.setLesson_start_time(req.getParameter("start"));
+		calendarDTO.setLesson_end_time(req.getParameter("end"));
 		
 		int result = calendarDAO.updateSchedule(calendarDTO);
 		if(result==1) {
@@ -68,13 +103,13 @@ public class CalendarController {
 		}
 		return "calendar.do";
 	}
-	
+		
 	@ResponseBody
 	@RequestMapping("/updateCalendar2" )
 	public String updateCalendar2(HttpServletRequest req, CalendarDTO calendarDTO){
 		
-		calendarDTO.setId(req.getParameter("id"));
-		calendarDTO.setEnd(req.getParameter("end"));
+		calendarDTO.setLesson_idx(req.getParameter("id"));
+		calendarDTO.setLesson_end_time(req.getParameter("end"));
 		
 		int result = calendarDAO.updateSchedule2(calendarDTO);
 		if(result==1) {
@@ -82,4 +117,5 @@ public class CalendarController {
 		}
 		return "calendar.do";
 	}
+	
 }
--- BodyCoding/src/main/webapp/WEB-INF/views/home.jsp
+++ BodyCoding/src/main/webapp/WEB-INF/views/home.jsp
@@ -7,46 +7,49 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h2>바디코딩 프로젝트</h2>
-	
-	<ul>
-		<li><a href="/">홈</a></li>
-	</ul>
+   <h2>바디코딩 프로젝트</h2>
+   
+   <ul>
+      <li><a href="/">홈</a></li>
+   </ul>
 
-	<!-- 어드민 로그인 하려면 main/admin으로 접속할 것 -->
-	<!-- <ul>
-		<li><a href="main/admin">admin 메인페이지</a></li>
-	</ul>
-	<ul>
-		<li><a href="/adminLogin.do">admin 로그인</a></li>
-	</ul> -->
-	<ul>
-		<li><a href="main">member 메인페이지</a></li>
-	</ul>
-	<ul>
-		<li><a href="calendar.do">캘린더</a></li>
-	</ul>
-	<ul>
-		<li><a href="trainer">트레이너 목록보기</a></li>
-	</ul>
-	<ul>
-		<li><a href="product">상품 목록보기</a></li>
-	</ul>
-	<ul>
-		<li><a href="pwcheck">마이페이지</a></li>
-	</ul>
-	<ul>
-		<li><a href="delete">탈퇴</a></li>
-	</ul>
-	<!-- 임시. 작업이후 삭제 예정 -->
-	<ul>
-		<li><a href="routine.do">운동루틴등록</a></li>
-	</ul>
-	<ul>
-		<li><a href="chart">차트</a></li>
-	</ul>
-	<ul>
-		<li><a href="room">채팅</a></li>
-	</ul>
+   <!-- 어드민 로그인 하려면 main/admin으로 접속할 것 -->
+   <!-- <ul>
+      <li><a href="main/admin">admin 메인페이지</a></li>
+   </ul>
+   <ul>
+      <li><a href="/adminLogin.do">admin 로그인</a></li>
+   </ul> -->
+   <ul>
+      <li><a href="main">member 메인페이지</a></li>
+   </ul>
+   <ul>
+      <li><a href="calendar.do">캘린더</a></li>
+   </ul>
+   <ul>
+      <li><a href="trainer">트레이너 목록보기</a></li>
+   </ul>
+   <ul>
+      <li><a href="product">상품 목록보기</a></li>
+   </ul>
+   <ul>
+      <li><a href="pwcheck">마이페이지</a></li>
+   </ul>
+   <ul>
+      <li><a href="delete">탈퇴</a></li>
+   </ul>
+   <!-- 임시. 작업이후 삭제 예정 -->
+   <ul>
+      <li><a href="routine.do">운동루틴등록</a></li>
+   </ul>
+   <ul>
+      <li><a href="supMainChart">sup차트</a></li>
+   </ul>
+   <ul>
+      <li><a href="subMainChart">sub차트</a></li>
+   </ul>
+   <ul>
+      <li><a href="room">채팅</a></li>
+   </ul>
 </body>
 </html>
